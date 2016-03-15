718a37b48d93af1ba177757374646c7a3e8db96f
erik tolentino and nic netzel <tolentino3575@gmail.com>
2016-03-15 10:16:38 -0700

    add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,7 @@ var lib = require('bower-files')({
     }
   }
 });
+var browserSync = require ('browser-sync').create();
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
@@ -79,3 +80,24 @@ gulp.task('bowerCSS', function(){
 });
 
 gulp.task('bower', ['bowerJS', 'bowerCSS']);
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+
+});
+
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browserSync.reload();
+});
+
+gulp.task('bowerBuild', ['bower'], function(){
+  broswerSynce.reload();
+});
