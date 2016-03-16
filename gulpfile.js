da0dab186c92566d5214195a0949683b0962e860
nic <netzeln@gmail.com>
2016-03-16 12:05:13 -0700

    add sass to skeleton branch

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,8 @@ var lib = require('bower-files')({
   }
 });
 var browserSync = require ('browser-sync').create();
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
@@ -58,6 +60,7 @@ gulp.task('build', ['clean'], function(){
     gulp.start('jsBrowserify');
   }
   gulp.start('bower');
+  gulp.start('cssBuild');
 });
 
 gulp.task('jshint', function(){
@@ -92,6 +95,7 @@ gulp.task('serve', function() {
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
   gulp.watch(['*.html'], ['htmlBuild']);
+  gulp.watch(["scss/*.scss"], ['cssBuild']);
 });
 
 gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
@@ -103,4 +107,13 @@ gulp.task('bowerBuild', ['bower'], function(){
 });
 gulp.task('htmlBuild', function(){
   browserSync.reload();
+});
+
+gulp.task('cssBuild', function() {
+  return gulp.src('scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'));
+    .pipe(browserSync.stream());
 });
