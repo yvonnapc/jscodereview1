112683874a62db1fcc8b1ac05c4a0a6aa2e14f4b
Staff <staff@staff-3.local>
2016-03-21 11:39:18 -0700

    Allow image to append by calling in browser-interface

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,9 +1,11 @@
 var getRepos = require("./../js/getRepos.js").getRepos;
+var getUserInfo = require("./../js/getRepos.js").getUserInfo;
 
 $(document).ready(function(){
   $('#search').submit(function(event){
     event.preventDefault();
     var searchedUser = $('#searchedUser').val();
     getRepos(searchedUser);
+    getUserInfo(searchedUser);
   });
 });
--- js/getRepos.js
+++ js/getRepos.js
@@ -3,6 +3,7 @@ var apiKey = require('./../.env').apiKey;
 exports.getRepos = function(searchedUser){
   $.get('https://api.github.com/users/' + searchedUser + '/repos?access_token=' + apiKey + '&per_page=1000').then(function(response){
     console.log(response);
+    $('#showInfo').empty();
     for(i=0; i<response.length; i++){
       var name = response[i].name;
       var description = response[i].description;
@@ -10,20 +11,20 @@ exports.getRepos = function(searchedUser){
       if(response[i].description === ""){
         var description = "N/A";
       }
-    $('#showInfo').prepend("<li>Repository name: " + name + "</li>" +
-                          "<ul><li>Description: " + description + "</li></ul>"+
-                          "<ul><li>Created At: " + date + "</li></ul>"+
-                           "<br>");
-  }
+      $('#showInfo').prepend("<li>Repository name: " + name + "</li>" +
+                            "<ul><li>Description: " + description + "</li></ul>"+
+                            "<ul><li>Created At: " + date + "</li></ul>"+
+                             "<br>");
+    }
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 };
 
-// exports.getUserInfo = function(searchedUser){
-//   $.get('https://api.github.com/users/' + searchedUser + '?access_token=' + apiKey).then(function(response){
-//     console.log(response);
-//     var image = response.avatar_url;
-//     $('#userInfo').append("<li><img src=" + image + "></li>");
-// });
-// };
+exports.getUserInfo = function(searchedUser){
+  $.get('https://api.github.com/users/' + searchedUser + '?access_token=' + apiKey).then(function(response){
+    console.log(response);
+    var image = response.avatar_url;
+    $('#userInfo').append("<li><img src=" + image + "></li>");
+  });
+};
